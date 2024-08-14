1b125d2749bc75cba7c36468eecb3005ccf027e6
RohithAndanala <andanala.rohith@gmail.com>
2024-08-14 13:36:21 -0400

    Added modal view for projects

--- src/assets/components/Projects.tsx
+++ src/assets/components/Projects.tsx
@@ -1,11 +1,16 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Modal, Row } from "react-bootstrap";
 import projectimg from "../projectImg.png";
-import Projectdetails from "./Projectdetails";
 import { useEffect, useState } from "react";
 
 function Projects() {
-  const url = "http://127.0.0.1:8000/project";
+  let jsonData = '{"title":"dummy", "description":"dummy description"}';
+  let dummy = JSON.parse(jsonData);
+
+  const url =
+    "http://127.0.0.1:8000/project/?skip=0&limit=100"; /* http://192.168.1.113:3456/project*/
   const [data, setData] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalData, setModalData] = useState(dummy);
 
   const fetchInfo = () => {
     return fetch(url)
@@ -18,28 +23,57 @@ function Projects() {
   }, []);
 
   return (
-    <Container>
-      <h1>Projects</h1>
-      <Row>
-        {data.map((d) => (
-          <Col className="d-flex justify-content-center">
-            <Card
-              className="projectile"
-              style={{
-                width: "25rem",
-                textAlign: "justify",
-                marginBottom: "10px",
-                alignItems: "center",
-              }}
-            >
-              <Card.Img src={projectimg} />
-              <Card.Title>{d.title}</Card.Title>
-              <Card.Text>{d.description}</Card.Text>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <h1>Projects</h1>
+        <Row>
+          {data.map((d) => (
+            <Col className="d-flex justify-content-center">
+              <Card
+                className="projectile"
+                style={{
+                  width: "25rem",
+                  textAlign: "justify",
+                  marginBottom: "10px",
+                  alignItems: "center",
+                }}
+              >
+                <Card.Img src={projectimg} />
+                <Card.Title>{d.title}</Card.Title>
+
+                <Button
+                  onClick={() => {
+                    setModalData(d), setModalIsOpen(true);
+                  }}
+                >
+                  Show More
+                </Button>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+      <Modal
+        show={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        dialogClassName="modal-20w"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {modalData.title}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Project Description</h4>
+          <p>{modalData.description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => setModalIsOpen(false)}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
