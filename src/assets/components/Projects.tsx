458c0f2cc5ec7298da08ec366686d40a55bcdbfe
RohithAndanala <andanala.rohith@gmail.com>
2024-08-13 23:58:19 -0400

    succesfully fected data from server

--- src/assets/components/Projects.tsx
+++ src/assets/components/Projects.tsx
@@ -1,15 +1,44 @@
 import { Card, Col, Container, Row } from "react-bootstrap";
 import projectimg from "../projectImg.png";
 import Projectdetails from "./Projectdetails";
+import { useEffect, useState } from "react";
 
 function Projects() {
-  const projects = Projectdetails();
+  const url = "http://127.0.0.1:8000/project";
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d));
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
   return (
     <Container>
       <h1>Projects</h1>
-      <a>
-        <pre>{JSON.stringify(projects)}</pre>
-      </a>
+      <Row>
+        {data.map((d) => (
+          <Col className="d-flex justify-content-center">
+            <Card
+              className="projectile"
+              style={{
+                width: "25rem",
+                textAlign: "justify",
+                marginBottom: "10px",
+                alignItems: "center",
+              }}
+            >
+              <Card.Img src={projectimg} />
+              <Card.Title>{d.title}</Card.Title>
+              <Card.Text>{d.description}</Card.Text>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 }
