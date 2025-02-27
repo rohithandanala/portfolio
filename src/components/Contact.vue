1a56aed072318ad0282ff3973b197650fe50b535
RohithAndanala <andanala.rohith@gmail.com>
2025-02-27 13:12:39 -0500

    added mobile version

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,32 +5,13 @@ export default {
 </script>
 
 <template>
-  <form id="Contact" class="SectionBox">
-    <h4 class="SectionHeading" style="color: black !important">Contact Me</h4>
-    <div class="form-group">
-      <label for="exampleFormControlInput1">Name</label>
-      <input type="text" class="form-control" placeholder="John Wick" />
+  <div class="SectionBox" id="ContactBox">
+    <h4 class="SectionHeading">Contact Details</h4>
+    <div>
+      <b>Email:</b><a> andanala.rohith357@gmail.com</a><br />
+      <b>Phone:</b><a> +1(571)-349-6408</a>
     </div>
-
-    <div class="form-group">
-      <label for="exampleFormControlInput1">Email address</label>
-      <input
-        type="email"
-        class="form-control"
-        id="exampleFormControlInput1"
-        placeholder="name@example.com"
-      />
-    </div>
-    <div class="form-group">
-      <label for="exampleFormControlTextarea1">Message</label>
-      <textarea
-        class="form-control"
-        id="exampleFormControlTextarea1"
-        rows="3"
-      ></textarea>
-    </div>
-    <button type="button" class="btn btn-dark">Submit</button>
-  </form>
+  </div>
 </template>
 
 <style></style>
