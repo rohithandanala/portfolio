5a18303ea514a0bef7d947da57760dde4a9788be
Andanala venkata sai rohith <45038560+rohithandanala@users.noreply.github.com>
2025-02-27 10:44:12 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"/portfolio/",
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
