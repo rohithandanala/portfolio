52a8acca2e31e0a84829b4e47e0beb79005031c8
RohithAndanala <andanala.rohith@gmail.com>
2025-02-28 21:31:30 -0500

    added certificate credentials

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -26,6 +26,12 @@ export default {
           <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />
           <b> Generative AI: The Evolution of Thoughtful Online Search </b>
           <i>issued by: LinkedIn</i>
+          <button class="btn btn-outline-light">
+            <a
+              href="https://www.linkedin.com/learning/certificates/6362b05e7189d6977062a852e6786ea53c568ea77fc0cbd0f7955756e2dbce3d"
+              >Show cerdentials</a
+            >
+          </button>
         </div>
         <!--div ends here-->
         <div class="certificate">
@@ -34,6 +40,12 @@ export default {
           />
           <b> Deep Learning A-Z™: Hands-On Artificial Neural Networks </b>
           <i>issued by: Udemy</i>
+          <button class="btn btn-outline-light">
+            <a
+              href="https://media.licdn.com/dms/image/v2/D4E2DAQE4Z6PYUzSdOA/profile-treasury-image-shrink_800_800/profile-treasury-image-shrink_800_800/0/1726326740717?e=1741402800&v=beta&t=bt5wdE0On2B1PMSCE29jo2WH1j2TRQn-QBZ_JGNQMOE"
+              >Show cerdentials</a
+            >
+          </button>
         </div>
         <!--div ends here-->
 
@@ -41,6 +53,12 @@ export default {
           <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />
           <b> Learning Microsoft 365 Copilot </b>
           <i>issued by: LinkedIn</i>
+          <button class="btn btn-outline-light">
+            <a
+              href="https://www.linkedin.com/learning/certificates/7a112051ed6dc46dc8ef997bfa4be7b1780131e4ce9d11391793efa47665b531"
+              >Show cerdentials</a
+            >
+          </button>
         </div>
         <!--div ends here-->
 
@@ -48,6 +66,12 @@ export default {
           <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />
           <b> Streamlining Your Work with Microsoft Copilot </b>
           <i>issued by: LinkedIn</i>
+          <button class="btn btn-outline-light">
+            <a
+              href="https://www.linkedin.com/learning/certificates/ec5005cc2754d6c562a4ef7d7dce404f50ef35d6e5d6a163d31d6d7eb54603a3"
+              >Show cerdentials</a
+            >
+          </button>
         </div>
         <!--div ends here-->
       </div>
