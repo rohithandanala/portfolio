658ce390ad8a1cc77f08b54a880d545729d75598
RohithAndanala <andanala.rohith@gmail.com>
2025-02-28 21:15:59 -0500

    added certification details

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -22,18 +22,34 @@ export default {
       <div id="certificationBox">
         <h5>Certification</h5>
         <br />
-        <b>A-Z Deeplearning </b><br />
-        <i>issued by: </i>
-        <hr />
-        <b>A-Z Deeplearning </b><br />
-        <i>issued by: </i>
-        <hr />
-        <b>A-Z Deeplearning </b><br />
-        <i>issued by: </i>
-        <hr />
-        <b>A-Z Deeplearning </b><br />
-        <i>issued by: </i>
-        <hr />
+        <div class="certificate">
+          <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />
+          <b> Generative AI: The Evolution of Thoughtful Online Search </b>
+          <i>issued by: LinkedIn</i>
+        </div>
+        <!--div ends here-->
+        <div class="certificate">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/046/437/277/non_2x/udemy-transparent-logo-free-png.png"
+          />
+          <b> Deep Learning A-Z™: Hands-On Artificial Neural Networks </b>
+          <i>issued by: Udemy</i>
+        </div>
+        <!--div ends here-->
+
+        <div class="certificate">
+          <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />
+          <b> Learning Microsoft 365 Copilot </b>
+          <i>issued by: LinkedIn</i>
+        </div>
+        <!--div ends here-->
+
+        <div class="certificate">
+          <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />
+          <b> Streamlining Your Work with Microsoft Copilot </b>
+          <i>issued by: LinkedIn</i>
+        </div>
+        <!--div ends here-->
       </div>
     </div>
   </div>
