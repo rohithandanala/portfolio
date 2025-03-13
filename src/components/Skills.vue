00d0c699b152b5f6b97b75e1b889a78e7b1ecdda
RohithAndanala <andanala.rohith@gmail.com>
2025-03-13 11:38:07 -0400

    updated skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -10,114 +10,43 @@ export default {
     <div id="skillcontainer">
       <div class="skill" id="skill1">
         <b>Artificial and Machine Learning</b>
-        <div class="progress">
-          <div
-            class="progress-bar bg-success"
-            role="progressbar"
-            style="width: 95%"
-            aria-valuenow="95"
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            95%
-          </div>
-        </div>
+
         <i><b>Technologies</b></i
         ><br />
-        <p>
-          Deep Learning, Generative Adversarial Network (GANs), Large Language
-          Models (LLM), Reinforcement Learning, Artificial Intelligence, Prompt
-          Engineering
-        </p>
+        <span class="skillobject">Deep Learning</span>
+        <span class="skillobject">Generative Adversarial Network (GANs)</span>
+        <span class="skillobject">Large Language Models (LLM)</span>
+        <span class="skillobject">Reinforcement Learning</span>
+        <span class="skillobject">Artificial Intelligence</span>
+        <span class="skillobject">Prompt Engineering</span>
       </div>
       <div class="skill" id="skill2">
-        <b>Full-Stack Development</b>
-        <div class="progress">
-          <div
-            class="progress-bar bg-success"
-            role="progressbar"
-            style="width: 80%"
-            aria-valuenow="80"
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            80%
-          </div>
-        </div>
-        <img
-          src="https://media.printables.com/media/prints/128175/images/1229540_1fa51ee6-6136-48b0-9c8b-44990845c1b4/thumbs/inside/1280x960/png/ec2-1.webp"
-        />
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/FastAPI_logo.svg/2560px-FastAPI_logo.svg.png"
-        />
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlAsNnw9xNv8jwWcYfwiK_oLTfyybkvXr8cw&s"
-        />
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7lNNEQjyOpW9yDxJYdm26ElgQm-fxlGKgmA&s"
-        />
-        <img
-          src="https://banner2.cleanpng.com/20180802/otq/acde0e8bfd6e7b24e04e1aa372b5d276.webp"
-        />
-        <img
-          src="https://static-00.iconduck.com/assets.00/arduino-icon-2048x1397-pmu0lemh.png"
-        />
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFFz9h8VijGKdjUZcrtuny7dWANuvqLNBu3Q&s"
-        />
-        <img
-          src="https://static-00.iconduck.com/assets.00/kafka-icon-2048x935-cvu4503l.png"
-        />
+        <b>Full-Stack Development</b><br />
+        <span class="skillobject">AWS EC2</span>
+        <span class="skillobject">Fast API</span>
+        <span class="skillobject">React JS</span>
+        <span class="skillobject">Vue JS</span>
+        <span class="skillobject">Android Studio</span>
+        <span class="skillobject">Arduino</span>
+        <span class="skillobject">.Net core</span>
+        <span class="skillobject">Kafka</span>
       </div>
 
       <div class="skill" id="skill3">
-        <b>Analytics</b>
-        <div class="progress">
-          <div
-            class="progress-bar bg-success"
-            role="progressbar"
-            style="width: 75%"
-            aria-valuenow="75"
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            75%
-          </div>
-        </div>
-        <img
-          src="https://www.radiansys.com/_next/static/media/GenerativeAIlogo.cfa666bc.svg"
-        />
-        <img
-          src="https://c8.alamy.com/comp/2X4G6W0/large-language-models-llm-symbol-vector-illustration-eps10-2X4G6W0.jpg"
-        />
+        <b>Analytics</b><br />
+        <span class="skillobject">Applied Statistics</span>
+        <span class="skillobject">Data Governance</span>
+        <span class="skillobject">Power BI</span>
+        <span class="skillobject">Matlab</span>
+        <span class="skillobject">TFIDF</span>
+        <span class="skillobject">Natural language processing (NLP)</span>
       </div>
 
       <div class="skill" id="skill4">
-        <b>database Management</b>
-        <div class="progress">
-          <div
-            class="progress-bar bg-success"
-            role="progressbar"
-            style="width: 80%"
-            aria-valuenow="80"
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            80%
-          </div>
-        </div>
-        <img
-          src="https://mariadb.com/wp-content/uploads/2019/11/mariadb-logo-vert_blue-transparent.png"
-        />
-        <img
-          src="https://1000logos.net/wp-content/uploads/2020/08/MongoDB-Logo.png"
-        />
-        <img
-          src="https://securiti.ai/wp-content/uploads/2023/07/Amazon-DynamoDB-Logo-large.webp"
-        />
-        <img
-          src="https://e7.pngegg.com/pngimages/617/252/png-clipart-mysql-workbench-computer-icons-logo-database-server-blue-text.png"
-        />
+        <b>database Management</b><br />
+        <span class="skillobject">MariaDB</span>
+        <span class="skillobject">MongoDB</span>
+        <span class="skillobject">DynamoDB</span>
       </div>
     </div>
   </div>
