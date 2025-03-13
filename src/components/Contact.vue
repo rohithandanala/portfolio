3686d063ad8a577fd2794be8b0103dd026df88b6
RohithAndanala <andanala.rohith@gmail.com>
2025-03-13 12:12:54 -0400

    updated contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -8,7 +8,7 @@ export default {
   <div class="SectionBox" id="ContactBox">
     <h4 class="SectionHeading">Contact Details</h4>
     <div>
-      <b>Email:</b><a> andanala.rohith357@gmail.com</a><br />
+      <b>Email:</b><a> andanalarohith3575@gmail.com</a><br />
       <b>Phone:</b><a> +1(571)-349-6408</a>
     </div>
   </div>
